504b4a0e91752282efccab61893cfd54c659849e
AmosAX <77676366+AmosAX@users.noreply.github.com>
2023-05-06 22:48:08 +0200

    Setting up the project and some tests
    Also commented in a grid to keep myself sane

--- src/main/java/MarsRoverImpl.java
+++ src/main/java/MarsRoverImpl.java
@@ -1,11 +1,22 @@
 public class MarsRoverImpl implements MarsRover {
 
+    int CurrentXPos;
+    int CurrentYPos;
+    Direction currentDirection;
+
+
     public MarsRoverImpl(Point2d startingPoint, Direction startingDirection){
+
+        this.CurrentXPos= startingPoint.x();
+        this.CurrentYPos= startingPoint.y();
+
+        this.currentDirection = startingDirection;
         //TODO implement
     }
 
     @Override
     public void move(String commands){
+
         //TODO implement
     }
 
--- src/test/java/MarsRoverTests.java
+++ src/test/java/MarsRoverTests.java
@@ -4,6 +4,32 @@ import org.junit.jupiter.api.Test;
 public class MarsRoverTests {
 
     @Test
+    public void testMoveForwardOneTime() {
+
+        //Arrange (see README for reference to Arrange-Act-Assert Pattern)
+        MarsRover rover = new MarsRoverImpl(new Point2d(3, 3), Direction.SOUTH);
+
+        //Act
+        rover.move("f");
+
+        //Assert
+        Assertions.assertEquals(new Point2d(3, 4), rover.getCurrentPosition());
+    }
+
+    @Test
+    public void testMoveBackOneTime() {
+
+        //Arrange (see README for reference to Arrange-Act-Assert Pattern)
+        MarsRover rover = new MarsRoverImpl(new Point2d(3, 3), Direction.SOUTH);
+
+        //Act
+        rover.move("b");
+
+        //Assert
+        Assertions.assertEquals(new Point2d(3, 2 ), rover.getCurrentPosition());
+    }
+
+    @Test
     public void testMoveForwardOneTimeLeftEdgeToRightEdge() {
 
         //Arrange (see README for reference to Arrange-Act-Assert Pattern)
@@ -16,3 +42,20 @@ public class MarsRoverTests {
         Assertions.assertEquals(new Point2d(5, 3), rover.getCurrentPosition());
     }
 }
+
+    /*         N
+       0   1   2   3   4
+     +---+---+---+---+---+
+    0|   |   |   |   |   |
+     +---+---+---+---+---+
+    1|   |   |   |   |   |
+     +---+---+---+---+---+
+  W 2|   |   |   |   |   | E
+     +---+---+---+---+---+
+    3|   |   |   |   |   |
+     +---+---+---+---+---+
+    4|   |   |   |   |   |
+     +---+---+---+---+---+
+               S
+
+ */
